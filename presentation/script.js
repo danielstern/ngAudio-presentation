5aae6d1c216002b419483e397f128ee713d1e0d5
unknown <daniel@danielstern.ca>
2014-10-20 19:57:02 -0400

    nice

--- presentation/script.js
+++ presentation/script.js
@@ -1,6 +1,6 @@
-angular.module("ngAudio.LiveDemo",['ngAudio'])
-.controller("DemoController",function($scope){
-    $scope.message = function() {
-        alert("VAMOS HACER UNO NEGOCIO");
-    }
-});
+// angular.module("ngAudio.LiveDemo",['ngAudio'])
+// .controller("DemoController",function($scope){
+//     $scope.message = function() {
+//         alert("VAMOS HACER UNO NEGOCIO");
+//     }
+// });
